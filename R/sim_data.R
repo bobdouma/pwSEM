1bc1d159c99d2505c8cf6f1477516dc0cd51ec3f
Bill Shipley <Bill.Shipley@USherbrooke.ca>
2023-12-01 10:23:47 -0500

    A second commit

--- R/sim_data.R
+++ R/sim_data.R
@@ -1,7 +1,7 @@
 #' sim_data:
 #' Simulated data with correlated errors involving endogenous
 #' variables and without any grouping structure.  Data generated
-#' using the following mixed acyclic graph:
+#' using this mixed acyclic graph:
 #' X1->X2->X3->X4 and X2<->X4
 #' @format ## 'sim_data'
 #' A data set with 100 rows and 4 columns
