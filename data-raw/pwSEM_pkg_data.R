c1b4bba02c28e455b49f1f6dbd2ed3c9938d6cdf
Bill Shipley <Bill.Shipley@USherbrooke.ca>
2024-02-01 17:02:21 -0500

    new sim_poisson.no.nesting

--- data-raw/pwSEM_pkg_data.R
+++ data-raw/pwSEM_pkg_data.R
@@ -23,7 +23,7 @@ usethis::use_data(sim_normal.no.nesting,overwrite=TRUE)
 set.seed(100)
 N<-100
 L<-stats::rnorm(N,0,2)
-X1<-stats::rnorm(N,0,0.4)
+X1<-stats::rnorm(N,0,2)
 lambda.X2<- 0.25*X1+0.25*L #This is the link function
 X2<-rpois(n=100,lambda=exp(lambda.X2))
 lambda.X3<-0.25*X2 #This is the link function
@@ -103,6 +103,7 @@ nested_data<-data.frame(year=blue.tits$year,nest=blue.tits$nest,
 pairs(nested_data)
 usethis::use_data(nested_data,overwrite = TRUE)
 
+#Example 1 of MEE paper
 set.seed(101)
 N<-500
 X1<-rnorm(N,0,1)
@@ -110,25 +111,54 @@ X2<-0.5*X1+rnorm(N,0,sqrt(1-0.5^2))
 X3<-0.5*X2+rnorm(N,0,sqrt(1-0.5^2))
 X4<-0.5*X3+rnorm(N,0,sqrt(1-0.5^2))
 dat1<-data.frame(X1,X2,X3,X4)
+library(ggm)
+dag1<-ggm::DAG(X2~X1,X3~X2,X4~X3)
+bs1<-ggm::basiSet(dag1)
+bs1
+R1<-residuals(mgcv::gam(X1~X2,data=dat1))
+R2<-residuals(mgcv::gam(X3~X2,data=dat1))
+generalized.covariance(R1,R2)$prob
+C1<- -2*sum(log(c(0.2916,0.5722,0.3302)))
+1-pchisq(C1,6)
 model.list<-list(mgcv::gam(X1~1,data=dat1,family=gaussian),
                  mgcv::gam(X2~X1,data=dat1,family=gaussian),
                  mgcv::gam(X3~X2,data=dat1,family=gaussian),
                  mgcv::gam(X4~X3,data=dat1,family=gaussian))
 out<-pwSEM(sem.functions=model.list,data=dat1,use.permutations=FALSE)
 summary(out,structural.equations=TRUE)
-#steps
-mag<-ggm::DAG(X2~X1,X3~X2,X4~X3)
-mag
-bs<-basiSet(mag)
-bs
-r1<-residuals(lm(X1~X2,data=dat1))
-r2<-residuals(lm(X3~X2,data=dat1))
-generalized.covariance(r1,r2)
-perm.generalized.covariance(r1,r2)
-summary(lm(X1~X2+X3,data=dat1))
-r1<-residuals(lm(X1~X3,data=dat1))
-r2<-residuals(lm(X4~X3,data=dat1))
-generalized.covariance(r1,r2)
-r1<-residuals(lm(X2~X1+X3,data=dat1))
-r2<-residuals(lm(X4~X1+X3,data=dat1))
-generalized.covariance(r1,r2)
+#Example 2 of MEE paper
+mag2.1<-ggm::DAG(X2~X1,X3~X2,X4~X3,order=TRUE)
+mag2.1[2,4]<-mag2.1[4,2]<-100
+mag2.1
+dag2<-CauseAndCorrelation::MAG.to.DAG(mag2.1)
+mag2.2<-CauseAndCorrelation::DAG.to.MAG(dag2,latents="L1")
+mag2.2
+pwSEM::basiSet.MAG(mag2.2)
+R1<-residuals(mgcv::gam(X1~X2,data=sim_normal.no.nesting))
+R2<-residuals(mgcv::gam(X3~X2,data=sim_normal.no.nesting))
+perm.generalized.covariance(R1,R2,nperm=10000)
+-2*log(0.0813)
+1-pchisq(5.0192,2)
+
+library(lavaan)
+lav.list<-'X2~X1
+               X3~X2
+               X4~X3
+               X2~~X4'
+summary(sem(lav.list,data=sim_normal.no.nesting))
+
+#Example 3 of MEE paper
+my.list<-list(mgcv::gam(X1~1,data=sim_poisson.no.nesting,family=gaussian),
+              mgcv::gam(X2~X1,data=sim_poisson.no.nesting,family=poisson),
+              mgcv::gam(X3~X2,data=sim_poisson.no.nesting,family=poisson),
+              mgcv::gam(X4~X3,data=sim_poisson.no.nesting,family=poisson))
+out<-pwSEM(sem.functions=my.list,dependent.errors=list(X4~~X2),
+           data=sim_poisson.no.nesting,use.permutations = TRUE,n.perms=10000)
+summary(out,structural.equations=TRUE)
+R1<-residuals(mgcv::gam(X1~X2,data=sim_poisson.no.nesting,family=gaussian))
+R2<-residuals(mgcv::gam(X3~X2,data=sim_poisson.no.nesting,family=poisson,type="working"))
+plot(R2~R1)
+perm.generalized.covariance(R1,R2,nperm=10000)
+-2*log(0.165)
+1-pchisq(3.6036,2)
+summary(mgcv::gam(X3~X2+X1,data=sim_poisson.no.nesting,family=poisson))
